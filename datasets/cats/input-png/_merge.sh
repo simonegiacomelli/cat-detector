8411d4471160f8b5bbcc88208658922c3817b592
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-09-20 19:22:52 +0200

    combine jpg image with mask to a transparent png

---
+++ datasets/cats/input-png/_merge.sh
@@ -0,0 +1,19 @@
+# convert jpg to png
+cd ../input
+mogrify -format png32 *.jpg
+for x in *.png32; do mv "$x" "${x%.png32}.png"; done
+mv *.png ../input-png
+
+# extract mask
+cd ../mask
+for x in *.jpg; do convert "$x" -background none -transparent black -flatten "${x%.jpg}.png" ; done
+mv *.png ../input-png
+cd ../input-png
+
+# merge and remove files
+for x in cat.*.png; do
+  convert $x mask_$x \
+          -alpha Off -compose CopyOpacity -composite \
+          $x
+  rm mask_$x
+done
