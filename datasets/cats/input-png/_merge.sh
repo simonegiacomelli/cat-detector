a8951f3c78d1b87ed2d1e54238bc29dbcfde4f73
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-09-21 17:38:27 +0200

    add dilate and trim to masks

--- datasets/cats/input-png/_merge.sh
+++ datasets/cats/input-png/_merge.sh
@@ -6,14 +6,14 @@ mv *.png ../input-png
 
 # extract mask
 cd ../mask
-for x in *.jpg; do convert "$x" -background none -transparent black -flatten "${x%.jpg}.png" ; done
+for x in *.jpg; do convert "$x" -background none -transparent black -flatten -morphology Dilate Octagon  "${x%.jpg}.png" ; done
 mv *.png ../input-png
 cd ../input-png
 
 # merge and remove files
 for x in cat.*.png; do
   convert $x mask_$x \
-          -alpha Off -compose CopyOpacity -composite \
+          -alpha Off -compose CopyOpacity -composite -trim +repage \
           $x
   rm mask_$x
 done
